6c44ae44d18bee82796e542a09eadc2bac43623b
Hasan Al Mamun <hasan@appnap.io>
2022-01-18 14:05:02 +0600

    MainActivity Crash Issues Resolved

--- app/src/main/java/com/hihasan/audioboo/views/MainActivity.kt
+++ app/src/main/java/com/hihasan/audioboo/views/MainActivity.kt
@@ -1,29 +1,31 @@
 package com.hihasan.audioboo.views
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.hihasan.audioboo.R
 import com.hihasan.audioboo.databinding.ActivityMainBinding
+import com.hihasan.audioboo.databinding.ContentMainBinding
+import com.hihasan.audioboo.utils.DialogUtil
 import com.hihasan.audioboo.utils.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : BaseActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ContentMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.content_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
